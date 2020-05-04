8df8893da68224fa6fc14d4d7f79c6bf2d403978
Matthew K <kleitz.matthew@gmail.com>
2020-05-03 23:05:50 -0400

    Added more models.

--- js/p5/header-sketch.js
+++ js/p5/header-sketch.js
@@ -2,10 +2,13 @@ let headerSketch = function(p) {
     let rotx = 0;
     let rotz = 0;
     let ilan, ilanTexture;
+    let bret, bretTexture;
 
     p.preload = function() {
         ilan = p.loadModel("models/ilan.obj");
-        ilanTexture = p.loadImage("models/ilantexture.png");
+        ilanTexture = p.loadImage("models/ilantexture2.png");
+        bret = p.loadModel("models/bret.obj");
+        bretTexture = p.loadImage("models/brettexture.png");
     }
 
     p.setup = function() {
@@ -32,14 +35,27 @@ let headerSketch = function(p) {
         rotz += 0.02 * (p.deltaTime / 50);
         p.pop();
 
+        p.pointLight(255, 255, 255, 100, 100, 250);
+
+        p.ambientMaterial(250);
         p.push();
         p.noStroke();
-        p.noFill();
         p.scale(200, -200);
-        p.rotateY(rotx);
+        p.rotateY(rotx/2);
         p.texture(ilanTexture);
         p.model(ilan);
         p.pop();
+
+        p.push();
+        p.noStroke();
+        p.scale(200, -200);
+        p.rotateY(rotx/3);
+        p.translate(2, 0);
+        p.texture(bretTexture);
+        p.model(bret);
+        p.pop();
+
+        
     }
 }
 new p5(headerSketch, 'sketch-container');
